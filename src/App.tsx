77a6e6860107273f3740c4109fff504d4b3cc40e
bence <67009276+bqnce@users.noreply.github.com>
2025-01-29 19:31:32 +0100

    Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import MainComponent from "./components/MainComponent";
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
     <>
       <div className="text-[#dbdbdb] flex justify-center items-center h-screen">
         <MainComponent />
+        <Analytics />
       </div>
     </>
   );
