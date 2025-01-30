ee0697e644a8cc5c1130b164b4ec5b4bcaa9da8d
bence <67009276+bqnce@users.noreply.github.com>
2025-01-30 18:17:46 +0100

    Deleted SpeedInsights and unnecessary/empty files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import MainComponent from "./components/MainComponent";
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 const App = () => {
@@ -10,7 +9,6 @@ const App = () => {
       <div className="text-[#dbdbdb] flex justify-center items-center h-screen">
         <MainComponent />
         <Analytics />
-        <SpeedInsights />
       </div>
     </>
   );
