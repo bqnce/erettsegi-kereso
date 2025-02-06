65901997ff7f20fe9180615da7c7375a91d014d3
bence <67009276+bqnce@users.noreply.github.com>
2025-02-06 21:29:05 +0100

    Removed button blured line from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,7 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   
   <StrictMode>
-          <div className='absolute w-screen top-0 h-[10px] bg-indigo-500 blur-2xl xl:hidden'></div>
-      <div className='absolute w-screen bottom-0 h-[5px] bg-indigo-400 blur-2xl xl:hidden'></div>
-
+    <div className='absolute w-screen top-0 h-[10px] bg-indigo-500 blur-2xl xl:hidden'></div>
     <App />
-
   </StrictMode>,
 )
