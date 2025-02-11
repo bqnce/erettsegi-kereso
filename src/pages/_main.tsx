195b29e91b2a7f9d1ccf7e5a77896dffe25f30f4
bence <67009276+bqnce@users.noreply.github.com>
2025-02-11 18:29:39 +0100

    Implemented Github button at the bottom

--- src/pages/_main.tsx
+++ src/pages/_main.tsx
@@ -1,7 +1,7 @@
 import { optionsTargy, optionsEv, optionsSzint, optionsIdoszak } from '../utils/options';
 import { useEffect, useState } from "react";
 import { fetchFile }  from '../utils/fetch';
-import { Search } from 'lucide-react';
+import { Search, Github } from 'lucide-react';
 import { generateUrls } from '../utils/generateUrls';
 import { CloudAlert } from 'lucide-react'
 import ButtonComponent from "../components/Button";
@@ -46,44 +46,51 @@ const MainComponent = () => {
     setIsZip(zipAvailable);
     setIsZipUt(zipUtAvailable);
   };
+
+  const openGithub = () => {
+    window.open("https://github.com/bqnce/erettsegi-kereso")
+  }
   
   return (
     <>
       <div className='h-auto w-screen md:h-[650px] md:w-[500px] rounded md:border-r md:border-l md:border-[#1f1f1f] overflow-hidden'>
-      <div className='h-[80px] flex justify-center items-center rounded-t border-t border-b border-[#1f1f1f]'>
-          <Header />
+        <div className='h-[80px] flex justify-center items-center rounded-t border-t border-b border-[#1f1f1f]'>
+            <Header />
         </div>
-        <div className='h-[490px] flex justify-center items-center gap-4 flex-col'>
-          <Combobox title="Tárgy" errorMsg="A tárgy nem elérhető" options={optionsTargy} onValueChange={setTargy}/>
-          <Combobox title="Év" errorMsg="Az év nem elérhető" options={optionsEv} onValueChange={setEv}/>
-          <Combobox title="Időszak" errorMsg="Az időszak nem elérhető" options={optionsIdoszak} onValueChange={setIdoszak}/>
-          <Combobox title="Szint" errorMsg="A szint nem elérhető" options={optionsSzint} onValueChange={setSzint}/>
-          <div className='mt-6'>
-            <ButtonComponent onClick={handleSearch} title="Keresés" disabled={isDisabled} icon={<Search className="mr-2 size-5"/>}/>
+          <div className='h-[490px] flex justify-center items-center gap-4 flex-col'>
+            <Combobox title="Tárgy" errorMsg="A tárgy nem elérhető" options={optionsTargy} onValueChange={setTargy}/>
+            <Combobox title="Év" errorMsg="Az év nem elérhető" options={optionsEv} onValueChange={setEv}/>
+            <Combobox title="Időszak" errorMsg="Az időszak nem elérhető" options={optionsIdoszak} onValueChange={setIdoszak}/>
+            <Combobox title="Szint" errorMsg="A szint nem elérhető" options={optionsSzint} onValueChange={setSzint}/>
+            <div className='mt-6'>
+              <ButtonComponent onClick={handleSearch} title="Keresés" disabled={isDisabled} icon={<Search className="mr-2 size-5"/>}/>
+            </div>
           </div>
-        </div>
-         <div className="sm:h-auto lg:h-[80px] p-4 rounded-b border-b border-t border-[#1f1f1f] flex justify-center items-center gap-3 flex-wrap">
-            {isTask && urls?.task ? (
-              <ButtonComponent title="Feladat" onClick={() => window.open(urls.task as string)} disabled={isDisabled} />
-            ) : (
-            <>
-            <CloudAlert color='#dc2626' />
-            <span className='font-normal bg-gradient-to-r bg-clip-text text-transparent from-red-600 via-red-500 to-red-400 animate-text'>Jelenleg nincs elérhető link</span>
-            </>
-            )}
-            {isGuide && urls?.guide && (
-              <ButtonComponent title="Útmutató" onClick={() => window.open(urls.guide as string)} disabled={isDisabled} />
-            )}
-            {isAudio && urls?.audio && (
-              <ButtonComponent title="Hanganyag" onClick={() => window.open(urls.audio as string)} disabled={isDisabled} />
-            )}
-            {isZip && urls?.zip && (
-            <Confirmation title="Forrás" onClick={() => window.open(urls.zip as string)} disabled={isDisabled} fileName={urls.zip}/> 
-            )}
-            {isZipUt && urls?.zipUt && (
-            <Confirmation title="Megoldás" onClick={() => window.open(urls.zipUt as string)} disabled={isDisabled} fileName={urls.zipUt}/> 
-            )}
-        </div>
+           <div className="sm:h-auto lg:h-[80px] p-4 rounded-b border-b border-t border-[#1f1f1f] flex justify-center items-center gap-3 flex-wrap">
+              {isTask && urls?.task ? (
+                <ButtonComponent title="Feladat" onClick={() => window.open(urls.task as string)} disabled={isDisabled} />
+              ) : (
+              <>
+              <CloudAlert color='#dc2626' />
+              <span className='font-normal bg-gradient-to-r bg-clip-text text-transparent from-red-600 via-red-500 to-red-400 animate-text'>Jelenleg nincs elérhető link</span>
+              </>
+              )}
+              {isGuide && urls?.guide && (
+                <ButtonComponent title="Útmutató" onClick={() => window.open(urls.guide as string)} disabled={isDisabled} />
+              )}
+              {isAudio && urls?.audio && (
+                <ButtonComponent title="Hanganyag" onClick={() => window.open(urls.audio as string)} disabled={isDisabled} />
+              )}
+              {isZip && urls?.zip && (
+              <Confirmation title="Forrás" onClick={() => window.open(urls.zip as string)} disabled={isDisabled} fileName={urls.zip}/> 
+              )}
+              {isZipUt && urls?.zipUt && (
+              <Confirmation title="Megoldás" onClick={() => window.open(urls.zipUt as string)} disabled={isDisabled} fileName={urls.zipUt}/> 
+              )}
+          </div>
+      </div>
+      <div className='p-2 rounded-lg border border-[#1f1f1f] hover:bg-[#1f1f1f] transition-colors duration-300 cursor-pointer absolute bottom-5' onClick={openGithub}>
+        <Github />
       </div>
     </>
   );
