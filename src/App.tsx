1148ace252ac509aa08da04865b72c729a9974f6
bence <67009276+bqnce@users.noreply.github.com>
2025-03-01 23:07:20 +0100

    Removed commented elements/imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainComponent from "./pages/_main";
-//import Login from './pages/_login.tsx'
-//import Register from "@/pages/_register.tsx";
 import "./App.css";
 
 const App = () => {
   return (
     <>
-      {/*<div
-        className={`text-[#dbdbdb] flex justify-center items-center min-h-[100vh] flex-col ${
-          darkMode ? "text-[#dbdbdb]" : "text-[#070707]"
-        }`}
-      >*/}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainComponent />} />
         </Routes>
       </BrowserRouter>
       <Analytics />
-      {/*</div>*/}
     </>
   );
 };
